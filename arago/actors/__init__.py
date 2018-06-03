4aad4503bd9a6d36114b64a41ee2440e07e17ebc
Marcus Klemm <mklemm@arago.de>
2018-06-03 23:19:04 +0200

    Unify various Exceptions

--- arago/actors/__init__.py
+++ arago/actors/__init__.py
@@ -1,3 +1,3 @@
-from arago.actors.actor import Actor, Task, ActorCrashedError, ActorStoppedError, ActorShutdownError
+from arago.actors.actor import Actor, Task, ActorStoppedError
 from arago.actors.monitor import Monitor, SHUTDOWN, RESTART, RESUME, ESCALATE, IGNORE, DEPLETE
 from arago.actors.router import Router
