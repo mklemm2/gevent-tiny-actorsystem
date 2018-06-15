21b6143a7b8098e7452b7ba92d92de6f28c9f84e
Marcus Klemm <mklemm@arago.de>
2018-06-15 14:59:35 +0200

    Don't route messages to stopped actors

--- arago/actors/router.py
+++ arago/actors/router.py
@@ -12,9 +12,13 @@ class Router(Monitor):
 			target = self._route(task.msg)
 			self._logger.trace("{me} is handing the task {task} to {target}".format(me=self, task=task, target=target))
 			return target._enqueue(task)
+		except ActorStoppedError as e:
+			gevent.idle()
+			self._logger.trace("{me} has failed to route {task} to {target} because {target} is stopped".format(me=self, task=task, target=target))
+			task.set_exception(e)
 		except Exception as e:
 			gevent.idle()
-			self._logger.trace("{me} has failed to route {task}".format(me=self, task=task))
+			self._logger.trace("{me} has failed to route {task}: Determining target failed".format(me=self, task=task))
 			task.set_exception(e)
 			raise
 
