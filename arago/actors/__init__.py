962c74029da1ced387ffb45ace2c96070a4a29c1
Marcus Klemm <mklemm@arago.de>
2018-06-19 10:18:15 +0200

    Add "Root" monitor to __init__.py for easier importing.

--- arago/actors/__init__.py
+++ arago/actors/__init__.py
@@ -1,3 +1,3 @@
 from arago.actors.actor import Actor, Task, ActorStoppedError
-from arago.actors.monitor import Monitor, SHUTDOWN, RESTART, RESUME, ESCALATE, IGNORE, DEPLETE
+from arago.actors.monitor import Monitor, Root, SHUTDOWN, RESTART, RESUME, ESCALATE, IGNORE, DEPLETE
 from arago.actors.router import Router
