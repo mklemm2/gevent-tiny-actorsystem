8184ff41aec98cb9da1f9324d2254c9a7e03f6a5
Marcus Klemm <mklemm@arago.de>
2018-07-03 16:03:36 +0200

    Update PingPong example to latest interface

--- ping_pong.py
+++ ping_pong.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python3
 from gevent import monkey; monkey.patch_all()
-import arago.actors.actor
+from arago.actors import Actor, Root
+from arago.common.logging import getCustomLogger
+import function_pattern_matching as fpm
+import sys, gevent
 
-class Ping(arago.actors.actor.Actor):
-	def handle(self, task):
-		print("{me} received {msg} from {other}.".format(me=self, msg=task.msg, other=task.sender))
-		if task.msg == "Pong":
-			task.sender.tell("Ping")
-		elif task.msg == "start":
-			self.opponent = Pong("PONG")
-			self.opponent.tell("Ping")
+def match(*args, **kwargs):
+        return fpm.case(fpm.guard(*args, **kwargs))
 
-class Pong(arago.actors.actor.Actor):
-	def handle(self, task):
-		print("{me} received {msg} from {other}.".format(me=self, msg=task.msg, other=task.sender))
-		if task.msg == "Ping":
-			task.sender.tell("Pong")
+logger = getCustomLogger(
+	level="TRACE", logfile=sys.stderr,
+	formatting=("%(asctime)s %(levelname)-7s %(message)s", "%Y-%m-%d %H:%M:%S")
+)
 
-ping = Ping("PING")
+class PingPong(Actor):
+	def aufschlag(self, opponent):
+		opponent.tell("Ping")
+	@match
+	def handle(self, msg: fpm.eq("Ping"), sender):
+		#gevent.sleep(2)
+		sender.tell("Pong")
+	@match
+	def handle(self, msg: fpm.eq("Pong"), sender):
+		#gevent.sleep(2)
+		sender.tell("Ping")
 
-ping.tell("start")
-ping.join()
+players = [PingPong(name="Player One"), PingPong(name="Player Two")]
+
+players[0].aufschlag(players[1])
+Root(name="root", children=players)
