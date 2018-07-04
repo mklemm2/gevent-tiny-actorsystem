c4ba38337c455d45ee203ed0495e83ce8ddf3da2
Marcus Klemm <mklemm@arago.de>
2018-07-04 19:22:08 +0200

    Adjust to latest interface changes

--- ping_pong.py
+++ ping_pong.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 from gevent import monkey; monkey.patch_all()
 from arago.actors import Actor, Root
+import arago.actors.pattern_matching as matching
 from arago.common.logging import getCustomLogger
-import function_pattern_matching as fpm
-import sys, gevent
-
-def match(*args, **kwargs):
-        return fpm.case(fpm.guard(*args, **kwargs))
+import operator
+import sys
 
 logger = getCustomLogger(
 	level="TRACE", logfile=sys.stderr,
@@ -15,15 +13,19 @@ logger = getCustomLogger(
 
 class PingPong(Actor):
 	def aufschlag(self, opponent):
-		opponent.tell("Ping")
-	@match
-	def handle(self, msg: fpm.eq("Ping"), sender):
-		#gevent.sleep(2)
+		opponent.tell("Ping", sender=self)
+
+	@matching.match(msg = "Ping", sender = matching.isoftype(Actor))
+	def handle(self, msg, payload, sender):
 		sender.tell("Pong")
-	@match
-	def handle(self, msg: fpm.eq("Pong"), sender):
-		#gevent.sleep(2)
+
+	@matching.match(msg = "Pong", sender = matching.isoftype(Actor))
+	def handle(self, msg, payload, sender):
 		sender.tell("Ping")
+
+	@matching.default
+	def handle(self, msg, payload, sender):
+		pass
 
 players = [PingPong(name="Player One"), PingPong(name="Player Two")]
 
