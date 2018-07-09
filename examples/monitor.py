1216566627fc9420a1362a859c3330103af6a1f5
Marcus Klemm <mklemm@arago.de>
2018-07-09 17:20:31 +0200

    Add basic monitor example

---
+++ examples/monitor.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from gevent import monkey; monkey.patch_all()
+from arago.actors import Actor, Monitor, Root, RESUME
+import arago.actors.pattern_matching as matching
+from arago.common.logging import getCustomLogger
+import gevent
+import random
+
+logger = getCustomLogger(level="DEBUG")
+
+class Echo(Actor):
+	@matching.match(msg = matching.isoftype(str))
+	def handle(self, msg, payload, sender):
+		return "{me} replies {msg}".format(me=self, msg=msg)
+
+def send(target):
+	for message in random.choices(["hello", 1], weights=[5,1], k=10):
+		try:
+			logger.info("Sending {msg} to {target}".format(msg=message, target=target))
+			answer = target.wait_for(message)
+			logger.info(answer)
+		except Exception as e:
+			logger.warning("Target raised an exception: {e}".format(e=e))
+
+echo = Echo(name="echo")
+monitor = Monitor(name="monitor", policy=RESUME, children=[echo])
+
+background = gevent.spawn(send, echo)
+background.join()
