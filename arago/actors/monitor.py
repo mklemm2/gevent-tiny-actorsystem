aaff866794243bd2c8425be0d4cfe48319a4d4ed
Marcus Klemm <mklemm@arago.de>
2019-07-29 14:05:52 +0200

    BUGFIX: use self._logger

--- arago/actors/monitor.py
+++ arago/actors/monitor.py
@@ -74,7 +74,7 @@ class Monitor(Actor):
 	def spawn_child(self, cls, *args, **kwargs):
 		"""Start an instance of cls(*args, **kwargs) as child"""
 		child = cls(*args, **kwargs)
-		self.logger.debug("{me} spawned new child {ch}".format(me=self, ch=child))
+		self._logger.debug("{me} spawned new child {ch}".format(me=self, ch=child))
 		self._register_child(child)
 
 	def register_child(self, child):
