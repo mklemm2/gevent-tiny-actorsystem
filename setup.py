d8de860233b88bc26f9198233fb464ccd6a1ed10
Marcus Klemm <mklemm@arago.de>
2019-09-10 12:58:29 +0200

    add 'function_pattern_matching' as requirement

--- setup.py
+++ setup.py
@@ -29,5 +29,5 @@ distutils.core.setup(
 			  'arago.actors.routers.shortest_queue',
 			  'arago.actors.sources.timer',
 	],
-	install_requires=['gevent']
+	install_requires=['gevent', 'function_pattern_matching']
 )
