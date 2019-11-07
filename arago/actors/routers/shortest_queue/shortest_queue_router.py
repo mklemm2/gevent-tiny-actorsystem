7498194ff1642dc4e3d8ae66330a5d81ce8f14a5
Marcus Klemm <mklemm@arago.de>
2019-11-07 12:31:22 +0100

    Try to find a free worker, first, before sorting by queue length.

--- arago/actors/routers/shortest_queue/shortest_queue_router.py
+++ arago/actors/routers/shortest_queue/shortest_queue_router.py
@@ -3,4 +3,11 @@ from arago.actors import Router
 class ShortestQueueRouter(Router):
 	"""Routes received messages to the child with the lowest number of enqueued tasks"""
 	def _route(self, msg):
-		return sorted(self._children, key=lambda item: len(item._mailbox), reverse=False)[0]
+		# Try to simply find a free worker, first
+		for item in self._children:
+			if hasattr(item, "_busy") and not item._busy:
+				return item
+		# If all workers are busy, order them by queue length and return the first
+		# FIXME: Can probably be optimized by not sorting the whole list
+		item = sorted(self._children, key=lambda item: len(item._mailbox), reverse=False)[0]
+		return item
